128a98cd0d7bb7d1b35983f5922fb6bee215b60f
fancojason <fancojason@gmail.com>
2026-07-14 09:23:47 +0800

    Add original seamless activewear trend guide

--- src/pages/sitemap-0.xml.ts
+++ src/pages/sitemap-0.xml.ts
@@ -46,6 +46,7 @@ const lastModifiedByPath = new Map<string, string>([
   ['/private-label/', '2026-07-10'],
   ['/products/', '2026-07-10'],
   ['/ready-to-ship/', '2026-07-10'],
+  ['/blogs/dfyne-style-activewear-manufacturing/', '2026-07-14'],
 ]);
 
 const getLastModified = (path: string) => {

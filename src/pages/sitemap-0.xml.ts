5346d14c72d30fbc54ca389b119c21f4a4cd5abb
fancojason <291067150+fancojason@users.noreply.github.com>
2026-09-22 21:29:30 +0800

    Add TZ2570-4 color block flare set product detail

--- src/pages/sitemap-0.xml.ts
+++ src/pages/sitemap-0.xml.ts
@@ -74,6 +74,7 @@ const productDetailPages = Object.keys(productModules)
   .map((slug) => `/products/${slug}/`);
 
 const lastModifiedByPath = new Map<string, string>([
+  ['/products/tz2570-4/', '2026-09-22'],
   ['/blogs/polka-dot-contrast-trim-activewear-wholesale/', '2026-09-22'],
   ['/es/fabricante-ropa-deportiva-espana/', '2026-09-13'],
   ...spanishGuides.map((guide): [string, string] => [`/es/guias/${guide.slug}/`, '2026-09-07']),
